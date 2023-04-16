7e95f99a1433d255fd2d8abf6ea1b52ef44898e3
Alexandra <alexa.alexa0511@gmail.com>
2023-04-16 22:47:41 +0200

    initial commit, install npm

---
+++ index.js
@@ -0,0 +1,20 @@
+const add = (a, b)=>{
+    const result = a + b;
+
+    return result;
+}
+add(2,2);
+
+const sub = (a, b)=>{
+    const result = a - b;
+
+    return result;
+}
+sub(3,2);
+
+const mult = (a, b)=>{
+    const result = a * b;
+
+    return result;
+}
+mult(2,2);
